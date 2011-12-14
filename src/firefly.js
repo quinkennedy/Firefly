edb70b5b5a167698955982fbe90aaf66a9555021
Quin Kennedy <quinkennedy@gmail.com>
2011-12-14 17:35:06 -0500

    initial commit, only splash screen displays

---
+++ src/firefly.js
@@ -0,0 +1,11 @@
+gbox.onLoad(function(){
+	help.akihabaraInit({
+		title:"Firefly",
+		splash:{footnotes:["Game Design and Art by David Frankel","Programming by Quin Kennedy"]}
+		});
+	
+	gbox.loadAll(go);
+}, false);
+
+function go(){
+}
