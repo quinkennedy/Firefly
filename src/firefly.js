f9b3679b76c687b93e4426a49b532f22abe39920
Quin Kennedy <quinkennedy@gmail.com>
2011-12-16 00:50:45 -0500

    added temp resources and slight changes to js file

--- src/firefly.js
+++ src/firefly.js
@@ -1,9 +1,16 @@
 gbox.onLoad(function(){
 	help.akihabaraInit({
 		title:"Firefly",
-		splash:{footnotes:["Game Design and Art by David Frankel","Programming by Quin Kennedy"]}
+		splash:{footnotes:["Game Design and Art by David Frankel","Programming by Quin Kennedy"], background:"resources/splash.png"}
 		});
-	
+
+	gbox.addImage("cels", "resources/cels.png");
+	gbox.addImage("scenery", "resources/gfx-village.png");
+	gbox.addTiles({id:"boy", image:"cels", tileh:30, tilew:30, tilerow:12, gapx:0, gapy:0});
+	gbox.addTiles({id:"firefly", image:"cels", tileh:10, tilew:10, tilerow:1, gapx:109, gapy:163});
+	gbox.addTiles({id:"will", image:"cels", tileh:30, tilew:30, tilerow:1, gapx:240, gapy:155});
+	gbox.addTiles({id:"bg", image:"scenery", tileh:30, tilew:30, tilerow:10, gapx:0, gapy:0});
+
 	gbox.loadAll(go);
 }, false);
 
