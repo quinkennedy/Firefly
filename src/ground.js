ceda45c8da72d311db62ccc6993e5f9acad7b1f4
Quin Kennedy <quinkennedy@gmail.com>
2012-02-26 13:17:27 -0500

    Added randomizing of tile light calculation sample point. This is not complete yet but it doesn't break anything and I wanted to get it into github, maybe it can be used for further discussion about the lighting.

--- src/ground.js
+++ src/ground.js
@@ -7,9 +7,18 @@ var Ground = function(x,y,name){
 		y:y,
 		split:1,
 		tilesize:30,
+		samplex:0,
+		sampley:0,
+		splitsamples:new Array(),
 		initialize:function(){
 			toys.topview.initialize(this,{
 				frames:{standup:{frames:[0]}}});
+			this.samplex = Math.random()*this.tilesize;
+			this.sampley = Math.random()*this.tilesize;
+			var step = this.tilesize / 3;
+			for(var i = 0; i < 9; i++){
+				this.splitsamples[i] = {x:Math.random()*step, y:Math.random()*step};
+			}
 		},
 		first:function(){
 			      toys.topview.handleAccellerations(this);
@@ -21,8 +30,8 @@ var Ground = function(x,y,name){
 				var playerX = pl.x+pl.hw;//canv.width/2;
 				var playerY = pl.y+pl.h-7;//canv.height/2;
 				var lightInfo = getLightInfo();
-				var myY = this.y + this.tilesize / 2;
-				var myX = this.x + this.tilesize / 2;
+				var myY = this.y + this.sampley;//this.tilesize / 2;
+				var myX = this.x + this.samplex;//this.tilesize / 2;
 				var myAmount, distance;
 
 				distance = Math.sqrt(Math.pow(playerX-myX,2)+Math.pow(playerY-myY,2));
@@ -32,17 +41,17 @@ var Ground = function(x,y,name){
 					var b = gbox.getObject("bug", entity);
 					distance = Math.sqrt(Math.pow(b.x+15-myX,2)+Math.pow(b.y+15-myY,2));
 					distance /= 30;
-					myAmount += (distance <= 1 ? b.brightness() : b.brightness()/Math.pow(distance,2));
+					myAmount += (distance <= 1 ? b.brightness()*2 : b.brightness()*2/Math.pow(distance,3));
 				}
 				var bottomI = this.getBottomI(myAmount);
 				this.actualblit(0,0,this.tilesize,bottomI,this.getRatio(myAmount));
-				if (bottomI <= 6){
-					var currSplit = 3;
+				if (bottomI <= 8){
+					var currSplit = 2;
 					var splitsize = this.tilesize / currSplit;
-					myY = this.y + splitsize / 2;
 					for(var h = 0; h < currSplit; h++){
-						myX = this.x + splitsize / 2;
 						for(var v = 0; v < currSplit; v++){
+							myY = this.y + (splitsize * h) + this.splitsamples[h*3+v].y;
+							myX = this.x + (splitsize * v) + this.splitsamples[h*3+v].x;
 							distance = Math.sqrt(Math.pow(playerX-myX,2)+Math.pow(playerY-myY,2));
 							distance /= 30;
 							myAmount = (distance <= 1 ? lightInfo.amount : lightInfo.amount/Math.pow(distance,2));
@@ -50,7 +59,7 @@ var Ground = function(x,y,name){
 								var b = gbox.getObject("bug", entity);
 								distance = Math.sqrt(Math.pow(b.x+15-myX,2)+Math.pow(b.y+15-myY,2));
 								distance /= 30;
-								myAmount += (distance <= 1 ? b.brightness() : b.brightness()/Math.pow(distance,2));
+								myAmount += (distance <= 1 ? b.brightness()*2 : b.brightness()*2/Math.pow(distance,3));
 							}
 							var bottomI = this.getBottomI(myAmount);
 							var ratio = this.getRatio(myAmount);
@@ -63,9 +72,7 @@ var Ground = function(x,y,name){
 								var yoffset = splitsize*h;
 								this.actualblit(xoffset,yoffset,splitsize, bottomI, ratio, topI);
 							}
-							myX += splitsize;
 						}
-						myY += splitsize;
 					}
 				}else{
 				}
